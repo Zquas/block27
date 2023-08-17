7fd24c3493440f96268a29ce7b36981bb73a9eb9
Jason Chen <114283388+Zquas@users.noreply.github.com>
2023-08-16 20:04:59 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+import { useState } from "react"
+import './App.css'
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  return (
+    <>
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
+    </>
+  );
+}
+
+export default App
+//npm run dev
